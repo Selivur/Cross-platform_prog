c930da6aaf7dcef43cd80a74c539b44698720973
Selivur <65359720+Selivur@users.noreply.github.com>
2022-10-16 20:00:42 +0300

    Add files via upload

---
+++ Cross2/Program.cs
@@ -0,0 +1,43 @@
+﻿string[] lines = System.IO.File.ReadAllLines
+        (@"C:\Users\si010\source\repos\Cross2\INPUT.txt");
+int count = int.Parse(lines[0]);
+int[] arr = new int[count];
+string[] stringArr = lines[1].Split(' ');
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = int.Parse(stringArr[i]);
+}    
+int firstPlayer=0;
+int secondPlayer=0;
+
+while (arr.Length > 0)
+{
+    if (arr[0] > arr[arr.Length - 1])
+    {
+        firstPlayer += arr[0];
+        arr = arr.Where((source, index) => index != 0).ToArray();
+    }
+    else
+    {
+        firstPlayer += arr[arr.Length - 1];
+        arr = arr.Where((source, index) => index != arr.Length - 1).ToArray();
+    }
+    if (arr.Length == 0)
+        break;   
+    if (arr[0] > arr[arr.Length - 1])
+    {
+        secondPlayer += arr[0];
+        arr = arr.Where((source, index) => index != 0).ToArray();
+    }
+    else
+    {
+        secondPlayer += arr[arr.Length - 1];
+        arr = arr.Where((source, index) => index != arr.Length - 1).ToArray();
+    }
+}
+if (firstPlayer == secondPlayer)
+    File.WriteAllText(@"C:\Users\si010\source\repos\Cross2\OUTPUT.txt", "0");
+else if (firstPlayer > secondPlayer)
+    File.WriteAllText(@"C:\Users\si010\source\repos\Cross2\OUTPUT.txt", "1");
+else
+    File.WriteAllText(@"C:\Users\si010\source\repos\Cross2\OUTPUT.txt", "2");
